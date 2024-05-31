17d4b9e3af7cefd03b918de8ae76454e825d5795
Atakan Temizkan <atikotemizkan@gmail.com>
2024-05-31 11:33:43 +0300

    İüğüğşğşğşöçöçöääßß

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-
 console.log(data);
 
-//You can start simple and just render a single 
+//You can start simple and just render a single
 //pokemon card from the first element
 console.log(data[0]);
+console.log(data[0].name);
+
+const UL = document.querySelector("ul");
+
+const CARD = document.createElement("li");
+CARD.className = "card";
+
+UL.appendChild(CARD);
+
+const LI = document.createElement("li");
+
+let test = Object.values(data[0])[4];
+
+console.log(test);
+
+console.log(data[0]);
+
+// LI.textContent = bruh[0];
+
+// CARD.appendChild(LI);
